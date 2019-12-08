3998afa3f095bab85d8be20c72d0a2d15a631f40
NguyenDQ <nguyencn11@gmailcom>
2019-12-08 15:13:13 +0700

    change and deploy iamge for Vu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  public canDisplay(): boolean {
+    const date: Date = new Date();
+
+    if (date.getDay() === 0  || date.getDay() === 6) {
+      return false;
+    }
+
+    const hours: number = date.getHours();
+
+    if ( !(hours >= 8 && hours <= 17)) {
+      return false;
+    }
+    return true;
+  }
 }
